5695c285aad1a32a4d44e75313f5080a4fc02a71
charkanihiba <114993403+Hibani@users.noreply.github.com>
2022-11-04 23:56:56 +0100

    Delete Test.py

--- Test.py
+++
@@ -1,8 +0,0 @@
-def fact(n):
-    p=1
-    for i in range(1,n+1):
-        p=p*i
-    return(p)
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print(fact(4))
